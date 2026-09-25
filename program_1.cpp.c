da2913fe89a695495be8f8d507d43cb1bc0ad772
Priyanshi Sachdev <priyanshisachdev07@gmail.com>
2026-09-25 22:20:48 +0530

    Add files via upload

---
+++ program_1.cpp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// Function
+int add(int a, int b){
+return a+b;
+}
+
+int main(void) {
+	// your code goes here
+int a = 10;
+int b = 20;
+int c = a+b;
+
+int a1 = 450;
+int b2 = 432;
+int c2 = a1+b2;
+printf("%d/n", c);
+printf("%d/n", c2);
+
+int result1 = add(10,50);
+int result2  = add(43,23);
+	return 0;
+}
